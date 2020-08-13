af437ab7d0c0d3662cc8652f6b6d9a5c3a5600a5
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-13 17:33:16 -0400

    added and messing around with theme objects

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+import Header from './ui/Header'
+import theme from './ui/Theme';
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
+      <Header />
       Hello!
     </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import Appbar from '@material-ui/core/Appbar';
+import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Typography from '@material-ui/core/Typography';
 
+
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 5 : 0,
+  });
+}
 
 export default function Header(props) {
     return(
-        <Appbar>
-            
-        </Appbar>
-    )
+        <ElevationScroll>
+        <AppBar position="fixed">
+            <Toolbar>
+                <Typography variant="h3">
+            Arc Development
+            </Typography>
+            </Toolbar>
+        </AppBar>
+        </ElevationScroll>
+    );
 }
