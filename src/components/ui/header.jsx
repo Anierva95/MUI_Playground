4e71b96201431661846360bc5f46cdb0d3d8a312
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-13 15:42:17 -0400

    create-react-app

---
+++ src/components/ui/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Appbar from '@material-ui/core/Appbar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+
+export default function Header(props) {
+    return(
+        <Appbar>
+            
+        </Appbar>
+    )
+}
