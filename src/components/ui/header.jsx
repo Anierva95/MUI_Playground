5170911bf162c0c187648aebf2bda2595b9d3e9a
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-17 16:49:19 -0400

    fixed estimate link

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -66,11 +66,17 @@ export default function Header(props) {
     else if (window.location.pathname === "/services" && value !== 1) {
       setValue(1);
     }
-    else if (window.location.pathname === "/revolution" && value !== 2) {
+    else if (window.location.pathname === "/about" && value !== 2) {
       setValue(2);
     }
-    else if (window.location.pathname === "/contact" && value !== 3) {
+    else if (window.location.pathname === "/revolution" && value !== 3) {
       setValue(3);
+    }
+    else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    }
+    else if (window.location.pathname === "/estimate" && value !== 4) {
+      setValue(4);
     }
   })
     return(
@@ -89,12 +95,13 @@ export default function Header(props) {
               >
                 <Tab component={Link} className={classes.tab} label="Home" to="/"/>
                 <Tab component={Link} className={classes.tab} label="Services" to="/services"/>
+                <Tab component={Link} className={classes.tab} label="About" to="/about"/>
                 <Tab component={Link} className={classes.tab} label="The Revolution" to="/revolution"/>
                 <Tab component={Link} className={classes.tab} label="Contact Us" to="/contact"/>
-            </Tabs>
-            <Button variant="contained" color="secondary" className={classes.button}>
+                <Button component={Link} variant="contained" to="/estimate" color="secondary" className={classes.button}>
               Free Estimate
             </Button>
+            </Tabs>
             </Toolbar>
 
         </AppBar>
