6f92de28ac6a24d0d8bad0d6b2935f82a61404de
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-18 13:12:56 -0400

    impported and started to implement the grid layout for the footer

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,23 +1,79 @@
-import React from 'react';
-import {makeStyles} from '@material-ui/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import Grid from "@material-ui/core/Grid";
+import footerAdornment from "../../assets/Footer Adornment.svg";
 
-import footerAdornment from '../../assets/Footer Adornment.svg';
-
-const useStyles = makeStyles(theme => ({
-    footer: {
-        backgroundColor: theme.palette.common.blue,
-        width: "100%"
-    },
-    adornment: {
-        width: "25em",
-        verticalAlign: "bottom"
-    }
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.common.blue,
+    width: "100%",
+  },
+  adornment: {
+    width: "25em",
+    verticalAlign: "bottom",
+  },
+  footerContainer: {
+      position: "absolute"
+  },
+  link: {
+      color: "white",
+      fontSize: ".75em",
+      fontFamily: "Arial",
+      fontWeight: "bold"
+  }
 }));
 
 export default function Footer(props) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return <footer className={classes.footer}>
-        <img alt="black slash" src={footerAdornment} className={classes.adornment}/>
-        </footer>
+  return (
+    <footer className={classes.footer}>
+      <Grid container direction="row" justify="center" className={classes.footerContainer}>
+          <Grid item>
+              <Grid container direction="column" className={classes.link}>
+                  <Grid item>
+                      Home
+                  </Grid>
+                  <Grid item>
+                      Home
+                  </Grid>
+                  <Grid item>
+                      Home
+                  </Grid>
+              </Grid>
+          </Grid>
+          <Grid item>
+              <Grid container direction="column" className={classes.link}>
+                  <Grid item>
+                      Home
+                  </Grid>
+                  <Grid item>
+                      Home
+                  </Grid>
+                  <Grid item>
+                      Home
+                  </Grid>
+              </Grid>
+          </Grid>
+          <Grid item>
+              <Grid container direction="column" className={classes.link}>
+                  <Grid item>
+                      Home
+                  </Grid>
+                  <Grid item>
+                      Home
+                  </Grid>
+                  <Grid item>
+                      Home
+                  </Grid>
+              </Grid>
+          </Grid>
+      </Grid>
+      <img
+        alt="black slash"
+        src={footerAdornment}
+        className={classes.adornment}
+      />
+    </footer>
+  );
 }
