55d10172e490350bcbbb978ad3d7c9de920b67a8
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-19 15:41:24 -0400

    refactored list so now it is mapped over

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import { makeStyles } from '@material-ui/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import React, { useState, useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
-
-import logo from '../../assets/logo.svg'
+import logo from "../../assets/logo.svg";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -25,133 +24,189 @@ function ElevationScroll(props) {
   });
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
-    marginBottom: "3em"
+    marginBottom: "3em",
   },
   logo: {
-    height: "8em"
+    height: "8em",
   },
   logoContainer: {
     padding: 0,
   },
   tabContainer: {
-    marginLeft: 'auto'
+    marginLeft: "auto",
   },
   tab: {
     ...theme.typography.tab,
     minWidth: 10,
-    marginLeft: "25px"
+    marginLeft: "25px",
   },
   button: {
     borderRadius: "50px",
     marginLeft: "50px",
     marginRight: "25px",
     fontSize: "1rem",
-    textTransform: "none"
+    textTransform: "none",
   },
   Menu: {
     backgroundColor: theme.palette.common.blue,
-    color: "white"
-  }
+    color: "white",
+    borderRadius: 0,
+  },
+  MenuItem: {
+    ...theme.typography.tab,
+    opacity: 0.7,
+    "&:hover": {
+      opacity: 1,
+    },
+  },
 }));
 
 export default function Header(props) {
-
   const classes = useStyles();
   const [value, setValue] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-    const handleChange = (e, value) => {
-      setValue(value);
-    }
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
 
-    const handleClick = (e) => {
-      setAnchorEl(e.currentTarget);
-      setOpen(true);
-    }
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
 
-    const handleClose = e => {
-      setAnchorEl(null);
-      setOpen(false);
-    }
+  const handleMenuItemClick = (e, index) => {
+    setAnchorEl(null);
+    setSelectedIndex(index);
+  }
+
+  const handleClose = (e) => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
+
+  const menuOptions = [
+    { name: "Services", link: "/services" },
+    { name: "Custom Software", link: "/customsoftware" },
+    { name: "Mobile Development", link: "/mobileapps" },
+    { name: "Website Development", link: "/websites" },
+  ];
 
   useEffect(() => {
     // Checking if the value does not match the pathname and fixing it
     if (window.location.pathname === "/" && value !== 0) {
       setValue(0);
-    }
-    else if (window.location.pathname === "/services" && value !== 1) {
+    } else if (window.location.pathname === "/services" && value !== 1) {
       setValue(1);
-    }
-    else if (window.location.pathname === "/about" && value !== 2) {
+    } else if (window.location.pathname === "/about" && value !== 2) {
       setValue(2);
-    }
-    else if (window.location.pathname === "/revolution" && value !== 3) {
+    } else if (window.location.pathname === "/revolution" && value !== 3) {
       setValue(3);
-    }
-    else if (window.location.pathname === "/contact" && value !== 4) {
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 4) {
       setValue(4);
     }
-    else if (window.location.pathname === "/estimate" && value !== 4) {
-      setValue(4);
-    }
-  }, [value])
-    return(
-      <>
-        <ElevationScroll>
+  }, [value]);
+  return (
+    <>
+      <ElevationScroll>
         <AppBar position="fixed">
-            <Toolbar disableGutters>
-            <Button disableRipple component={Link} to="/" className={classes.logoContainer} onClick={() => setValue(0)} >
-                <img src={logo} className={classes.logo}/>
-                </Button>
+          <Toolbar disableGutters>
+            <Button
+              disableRipple
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+            >
+              <img src={logo} className={classes.logo} />
+            </Button>
 
-              <Tabs 
-              value={value} 
-              onChange={handleChange} 
+            <Tabs
+              value={value}
+              onChange={handleChange}
               className={classes.tabContainer}
               indicatorColor="primary"
-              >
-                <Tab component={Link} className={classes.tab} label="Home" to="/"/>
+            >
+              <Tab
+                component={Link}
+                className={classes.tab}
+                label="Home"
+                to="/"
+              />
 
-                <Tab 
-                aria-owns={anchorEl ? "simple-menu" : undefined} 
-                component={Link} 
+              <Tab
+                aria-owns={anchorEl ? "simple-menu" : undefined}
+                component={Link}
                 className={classes.tab}
                 aria-haspopup={anchorEl ? "true" : undefined}
                 onMouseOver={(event) => handleClick(event)}
-                label="Services" to="/services"/>
+                label="Services"
+                to="/services"
+              />
 
-                
-                <Tab component={Link} className={classes.tab} label="About" to="/about"/>
-                <Tab component={Link} className={classes.tab} label="The Revolution" to="/revolution"/>
-                <Tab component={Link} className={classes.tab} label="Contact Us" to="/contact"/>
+              <Tab
+                component={Link}
+                className={classes.tab}
+                label="About"
+                to="/about"
+              />
+              <Tab
+                component={Link}
+                className={classes.tab}
+                label="The Revolution"
+                to="/revolution"
+              />
+              <Tab
+                component={Link}
+                className={classes.tab}
+                label="Contact Us"
+                to="/contact"
+              />
             </Tabs>
-            <Button component={Link} variant="contained" to="/estimate" color="secondary" className={classes.button}>
+            <Button
+              component={Link}
+              variant="contained"
+              to="/estimate"
+              color="secondary"
+              className={classes.button}
+            >
               Free Estimate
             </Button>
-            <Menu 
-            id="simple-menu"
-            classes={{paper: classes.Menu}}
-            anchorEl={anchorEl} 
-            open={open} 
-            onClose={handleClose}
-            MenuListProps={{onMouseLeave: handleClose}}
-            onChange={handleChange} 
-            elevation={0}
+            <Menu
+              id="simple-menu"
+              classes={{ paper: classes.Menu }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{ onMouseLeave: handleClose }}
+              onChange={handleChange}
+              elevation={0}
             >
-              <MenuItem to="/customsoftware" component={Link} onClick={() => {handleClose(); setValue(1)}}>Services</MenuItem>
-              <MenuItem to="/customsoftware" component={Link} onClick={() => {handleClose(); setValue(1)}}>Custom Software</MenuItem>
-              <MenuItem to="/customsoftware" component={Link} onClick={() => {handleClose(); setValue(1)}}>Mobile Applications</MenuItem>
-              <MenuItem to="/customsoftware" component={Link} onClick={() => {handleClose(); setValue(1)}}>Website Development</MenuItem>
+              {menuOptions.map((option, index )=> {
+                return(
+                  <MenuItem 
+                  classes={{ root: classes.MenuItem}}
+                  to={option.link}
+                  component={Link}
+                  onClick={() => {handleClose(); setValue(1); handleMenuItemClick()}}
+                  selected={selectedIndex === index}
+                  >
+                  {option.name}
+                  </MenuItem>
+                )
+              })}
             </Menu>
-            </Toolbar>
-
+          </Toolbar>
         </AppBar>
-        </ElevationScroll>
-        <div className={classes.toolbarMargin}/> 
-        </>
-    );
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
+  );
 }
