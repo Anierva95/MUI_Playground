9f1453238673b550389f5ce31e844511dc4fbe2f
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-29 23:42:27 -0400

    finishing up CTA

--- src/components/ui/CTA.jsx
+++ src/components/ui/CTA.jsx
@@ -26,3 +26,36 @@ const useStyles = makeStyles((theme) => ({
     }
   }));
   
+
+  export default function CTA(props) {
+
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+
+    const classes = useStyles();
+  
+    return (
+        <>
+        <Grid container direction="row" justify="center" alignItems="baseline" className={classes.footerContainer}>
+            <Grid item className={classes.gridItem}>
+                <Grid container direction="column" className={classes.link}>
+                    <Grid item>
+                        Are you ready to start your journey?
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item className={classes.gridItem}>
+                <Grid container direction="column" className={classes.link} spacing={2}>
+                    <Grid item>
+                        We are happy to help!
+                    </Grid>
+                    </Grid>
+
+      </>
+    );
+  }
+  
